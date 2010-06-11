4d49b7f7570894a58a0749ebb850afb0de243c9c
Rob Biedenharn <Rob@AgileConsultingLLC.com>
2010-06-11 10:46:49 -0400

    New Cratchit project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = 'ad68c5e033a754a7d7239b038685690b827222f09cb7c87e8eccd31cd6606ef65d56bacbed21c983eae8124800ab8b6f3b73ee2c8074d0f3179de2ae0ffe45be'
